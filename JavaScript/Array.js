be53a9e09e55f414f7733d03ef1cc8c64252669d
Tony <un_razum@mail.ru>
2022-12-17 14:16:31 +0300

    Create Array.js

---
+++ JavaScript/Array.js
@@ -0,0 +1,17 @@
+let array = []
+let string = "abc"
+let res;
+array = string.split("");
+
+console.log(array)
+console.log(res)
+
+res = array.join()
+
+console.log(array)
+console.log(res)
+
+res = array.reverse()
+
+console.log(array)
+console.log(res)
