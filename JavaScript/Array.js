99fbabbc26b86092d1807adb526dc04ad4d5dc2d
Tony <un_razum@mail.ru>
2023-01-04 11:41:17 +0300

    Update Array.js
    add func printArray

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -15,3 +15,14 @@ res = array.reverse()
 
 console.log(array)
 console.log(res)
+
+function printArray(a){
+  var len = a.length, i = 0;
+  if(len == 0)
+    console.log("Empty Array");
+  else
+  { do {
+    console.log(a[i]);
+  } while (++i < len);
+  }
+}
