9be60df63f5c89ec41c8bdda76a38f8e7424b8f5
Tony <un_razum@mail.ru>
2023-01-04 17:21:01 +0300

    feat: add some line code

---
+++ React/testing-app/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+// function App() {
+//   function getName(){
+//     return "Tony"
+//   }
+//   var myStyle = {
+//     color:"green",
+//     fontSize:100
+//   }
+//   // var myname = "Tony";
+//   // var elem = <h3>Just try it again</h3>;
+//   return (
+    
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <h1>Hello World!!!!!</h1> */}
+//         <h1>Hello {2+2}</h1>
+//         {/* <h1>Hello {myname}</h1> */}
+//         {/* <h2>Hello {this.getName()}</h2> */}
+//         <h2 style = {myStyle} data-myattribute="somevalue">Hello {getName()}</h2>
+//         {/* <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// class Hello extends Component{
+//   render(){
+//     return <h1> Hello oooo </h1>
+//   }
+// }
+
+// export default Hello;
