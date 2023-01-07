e395ca69de6e54f26d716c86a6cd20ec90341260
Tony <un_razum@mail.ru>
2023-01-07 13:53:39 +0300

    feat: add example method reduce()

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -25,6 +25,17 @@ console.log(res)
 // var output = fs.readFileSync('data.txt') // Buffer
 var output = fs.readFileSync('data.txt', 'utf8')
     .trim()
-    // .split('\n')
-    // .map(line => line.split('\t'))
-console.log("output :",output)
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce((customers,line) => {
+        // console.log(line)
+        customers[line[0]] = customers[line[0]] || [];
+        customers[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        }) ;
+        return customers;
+    },{})
+// console.log("output :",output)
+console.log("output :", JSON.stringify(output, null , 2))
