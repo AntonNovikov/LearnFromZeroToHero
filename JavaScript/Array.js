7e31e164b52569cc8252a9e61e9ceff45bef5040
Tony <un_razum@mail.ru>
2023-01-07 12:49:44 +0300

    feat: i want to sleep

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,3 +1,5 @@
+var fs = require("fs")
+// import fs from 'fs'
 // map filter reject reduce join reverse find
 let array = []
 let string = "abc"
@@ -16,3 +18,13 @@ res = array.reverse()
 
 console.log(array)
 console.log(res)
+
+
+
+// https://www.youtube.com/watch?v=1DMolJ2FrNY&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84&index=4
+// var output = fs.readFileSync('data.txt') // Buffer
+var output = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    // .split('\n')
+    // .map(line => line.split('\t'))
+console.log("output :",output)
