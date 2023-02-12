d32ff0350665d0bd8550886fc52bf9cdfa4eb9b1
Tony <un_razum@mail.ru>
2023-02-12 20:31:12 +0300

    refactor: move some lines code

--- Python/book/bookB.py
+++ Python/book/bookB.py
@@ -10,8 +10,14 @@
 
 # 19
 
+if(1%2): # True or False
+    print("true 1")
+# for counter in range(10):
 for counter in range(2,12,3):
-    if counter % 2 == 0 :
+    # if counter % 2 == 0 :
+    # if (counter % 2 == 0) :
+    # if (counter % 2) == 0 :
+    if ((counter % 2) == 0) :
         print(counter)
         print("even (четное)")  
         print("")  
@@ -19,3 +25,14 @@ for counter in range(2,12,3):
         print(counter)
         print("odd (нечетное))")
         print("")
+
+
+
+
+for counter in range(10):
+    if (counter % 2 == 0 ):
+        print(counter)
+        print("odd")
+    else:
+        print(counter)
+        print("even")    
