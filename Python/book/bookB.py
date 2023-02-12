7e9e247b6fcaf9ff77cce2739babf741384d63c4
Tony <un_razum@mail.ru>
2023-02-12 20:08:05 +0300

    refactor: replace function

---
+++ Python/book/bookB.py
@@ -0,0 +1,21 @@
+# 12
+
+# print(3+3)
+
+# 18
+
+# print("Мне 34 годиков")
+# print(123+456*7/8)
+# print(''.join(reversed("Время программировать")))
+
+# 19
+
+for counter in range(2,12,3):
+    if counter % 2 == 0 :
+        print(counter)
+        print("even (четное)")  
+        print("")  
+    else:
+        print(counter)
+        print("odd (нечетное))")
+        print("")
--- Python/book/k.py
+++
@@ -1,8 +0,0 @@
-
-for counter in range(2,12,3):
-    if counter % 2 == 0 :
-        print(counter)
-        print("odd")
-    else:
-        print(counter)
-        print("even")  
