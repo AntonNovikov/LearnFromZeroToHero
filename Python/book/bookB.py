e284db7e5241bae1c9fdbdd29ed3c7e678673e31
Tony <un_razum@mail.ru>
2023-02-13 21:05:51 +0300

    feat: crazy

--- Python/book/bookB.py
+++ Python/book/bookB.py
@@ -19,33 +19,100 @@
 #     # if (counter % 2) == 0 :
 #     if ((counter % 2) == 0) :
 #         print(counter)
-#         print("even (четное)")  
-#         print("")  
+#         print("even (четное)")
+#         print("")
 #     else:
 #         print(counter)
 #         print("odd (нечетное))")
 #         print("")
 
 
-# 22 
+# 22
 
-#
-# name = " ино аоо"
-# greeting = "hello, "
-# message = name + greeting
+# print("Hello, world")
+# # name = input('How are you?')
+# name = "Tony"
+# print(' Hello one, ', name)
+
+
+# greeting = "Hello, "
+# message = greeting + name
 # print(message)
+# print("lenght of string: " + str((len(message))), len(message))  # 2 varieties
+
 # a = 10
 # b = 4
-# print(a<b) # false это 
-# age = 10
-# if age == 10:
-#     print('Hello world')
+# print(a < b)  # false это
+
+# # 27
+
+# fruits = ["orange", "apple", "pear"]
+# boy_names = ['Alex', "Bob", "Henry II"]
+# # https://www.verywellfamily.com/top-1000-baby-girl-names-2757832
+# girl_names = ["Olivia", "Emma", "Charlotte",
+#               "Amelia", "Ava", "Sophia", "Isabella", "Mia"]
+# print(boy_names[0])
+
 # страница 29
 
-# == равно
-# != не равно
-# < меньше, чем
-# > больше, чем
-is_dark = input('На улице темно? yes / no )')
-if is_dark == 'yes':
-    print('Спокойной ночи! Хр-р-р-р-р-р.. . ')
+# # boolean
+# true = True
+# false = False
+# trueToo = not(False)
+# print(trueToo)
+# age = 10
+# print(age)
+# if age == 10:
+#     print('Hello world')
+# == equals
+# != not equals
+# < less, than
+# > more, than
+# is_dark = input('I's time to sleeppp? yes / no )')
+# is_dark = 'yes'
+# if is_dark == 'yes':
+#     print('good  niby bybb yb! zz/zZzz/Z/zzz. ')
+# and
+# age = 21
+# hasDriverLicense = True
+# if age >= 21 and (hasDriverLicense):
+#     print("you may keep drive away")
+
+# 32
+
+# for item in range (1,10,2):
+#     print(item)
+# question = 0
+# answer = "yes"
+# while answer == "yes":
+#     question = question +1
+#     answer = input("Do you want any question more? (yes/yes)")
+
+def check_answer(answerIn, answer):
+    global score
+    isNotAnswered = True
+    attempt = 0
+    while isNotAnswered and attempt < 3:
+        if answerIn.upper() == answer.upper():
+            print("it's right... god...")
+            score = score + 3 - attempt
+            isNotAnswered = False
+        else:
+            if attempt < 2:
+                answerIn = input("You don't know.. try again...")
+            attempt = attempt + 1
+    if attempt == 3:
+        print("Almost...See here... answer is ", answer)
+
+
+score = 0
+english_words = ("Cat", "Dog")
+russian_words = ("Кошка", "Собака")
+print("Test your English")
+
+answerIn = input("Translate to Russian " + english_words[0] + ": ")
+check_answer(answerIn, russian_words[0])
+
+print("Your score: ", score)
+
+# 45
