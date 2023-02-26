254bdbcb4960aef3bdebcf885fe98e11e642cc40
Tony <un_razum@mail.ru>
2023-02-26 21:44:17 +0300

    feat:  comment code for use

--- JavaScript/sololearn/challenge.js
+++ JavaScript/sololearn/challenge.js
@@ -1,31 +1,32 @@
-var y = "John";
-var x = +y; // NaN
-console.log(typeof x); // number
+// var y = "John";
+// var x = +y; // NaN
+// console.log(typeof x); // number
 
-// by Brains[Abidemi]
-const city = {
-  spain: "madrid",
-  usa: "new-york",
-  india: "punjab",
-};
-const city2 = Object.freeze(city);
-// city.spain = "braca";
-console.log((city == city2) + " " + city.spain);
+// // by Brains[Abidemi]
+// const city = {
+//   spain: "madrid",
+//   usa: "new-york",
+//   india: "punjab",
+// };
+// const city2 = Object.freeze(city);
+// // city.spain = "braca";
+// console.log((city == city2) + " " + city.spain);
 
-// by Ivan
-var arr = [1, 2, 3, 4, 5, 6, 7];
-arr.length = 3;
-console.log(arr.length);
+// // by Ivan
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.length = 3;
+// console.log(arr.length);
 
-//by Alquen
-var x = 1;
-var y = 4;
-function func() {
-  // it's not invoke
-  while (y > 0) {
-    x++;
-    y--;
-  }
-}
-// there isn't invoke
-console.log(x);
+// //by Alquen
+// var x = 1;
+// var y = 4;
+// function func() {
+//   // it's not invoke
+//   while (y > 0) {
+//     x++;
+//     y--;
+//   }
+// }
+// // there isn't invoke
+// console.log(x);
+
