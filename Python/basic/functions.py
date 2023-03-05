b99d7429ffcdbe015b3560d20e602bf37e4ae426
Tony <un_razum@mail.ru>
2023-03-05 12:10:37 +0300

    feat: add some function

--- Python/basic/functions.py
+++ Python/basic/functions.py
@@ -1,1 +1,10 @@
 # https://pythonru.com/osnovy/funkcija-range-v-python
+
+# strip()
+# https://www.w3schools.com/python/ref_string_strip.asp
+
+txt = ",,,,,rrttgg.....banana....rrr"
+
+x = txt.strip(",.grt")
+
+print(x)
