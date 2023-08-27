2bd6aadbe933576f40ed34f49dcdc564a9aa6d46
Tony <un_razum@mail.ru>
2023-08-27 15:48:35 +0300

    feat: add method

--- vue3/Academind/basics/app.js
+++ vue3/Academind/basics/app.js
@@ -5,6 +5,16 @@ data(){
         vueLink: 'https://vuejs.org'
 
     }
+},
+methods: {
+    outputGoal(){
+        const randomNumber = Math.random()
+        if(randomNumber< 0.5){
+            return 'Learn Vue!';
+        } else {
+            return 'Master Vue!'
+        }
+    }
 }
 })
 
