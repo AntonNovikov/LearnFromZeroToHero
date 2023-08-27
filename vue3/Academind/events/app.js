6b22bad266219c2f53b35092d13397f94e156a5c
Tony <un_razum@mail.ru>
2023-08-27 19:06:30 +0300

    feat: learn about v-once

--- vue3/Academind/events/app.js
+++ vue3/Academind/events/app.js
@@ -1,9 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name : '',
+      confirmedName:'',
     };
   },
+  methods: {
+    confirmInput(){
+      this.confirmedName = this.name
+    },
+    submitForm(){
+      // event.preventDefault();
+alert("submit")
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add() {
+      this.counter = this.counter + 1;
+    },
+    reduce() {
+      this.counter = this.counter - 1;
+      // this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
