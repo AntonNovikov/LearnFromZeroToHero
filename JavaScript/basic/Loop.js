4eb577da3f0f8fda67dc158c5fbf1d6e7bf31760
Tony <un_razum@mail.ru>
2023-09-09 19:58:09 +0300

    feat: add some info about for

--- JavaScript/basic/Loop.js
+++ JavaScript/basic/Loop.js
@@ -2,27 +2,49 @@
 
 // for
 
+for (let i = 9; i < 5; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 5; ) {
+  console.log(1);
+  i++;
+}
+for (let i = 0; ; ) {
+  if (i < 5) {
+    console.log(1);
+  } else {
+    break;
+  }
+  i++;
+}
+let i = 0;
+for (;;) {
+  if (i < 5) {
+    console.log(i);
+  } else {
+    i = 0;
+    break;
+  }
+  i++;
+}
+
 // for in
 
 for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
-    }
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+  }
 }
 
 // for of
 
 for (const iterator of object) {
-    
 }
 
 // while
 
 // do while
 
-
 // break
 
 // continue
-
