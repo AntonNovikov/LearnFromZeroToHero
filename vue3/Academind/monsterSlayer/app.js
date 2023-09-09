4d4c3b4be2fea753293dd1b56f9c16b83896a72e
Tony <un_razum@mail.ru>
2023-09-09 22:23:26 +0300

    feat: add attack funciton

--- vue3/Academind/monsterSlayer/app.js
+++ vue3/Academind/monsterSlayer/app.js
@@ -1,23 +1,44 @@
 function getRandomValue(min, max) {
-  Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-Vue.create({
+Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
       playerHit: [5, 12],
-      mosterHit: [8, 15],
+      monsterHit: [8, 15],
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
   },
   methods: {
     attackMonster() {
+      console.log(this.monsterHealth);
+      console.log(this.playerHit[0], this.playerHit[1]);
       const attackValue = getRandomValue(this.playerHit[0], this.playerHit[1]);
+      // const attackValue = getRandomValue(5, 12);
+      console.log("++attackValue");
+      console.log(attackValue);
+
       this.monsterHealth = this.monsterHealth - attackValue;
+      console.log(this.monsterHealth);
+      this.attackPlayer();
     },
     attackPlayer() {
-      const attackValue = getRandomValue(this.mosterHit[0], this.mosterHit[1]);
+      const attackValue = getRandomValue(
+        this.monsterHit[0],
+        this.monsterHit[1]
+      );
+      console.log("++attackValue to Player");
+      console.log(attackValue);
       this.playerHealth = this.playerHealth - attackValue;
     },
   },
