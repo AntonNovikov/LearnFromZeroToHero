1639c5c11ec385315d38cfff08a105456d0046f2
Tony <un_razum@mail.ru>
2023-09-10 20:32:38 +0300

    feat: add first cli vue app

---
+++ vue3/README.md
@@ -0,0 +1,23 @@
+
+useful links
+cli.vuejs.org
+nodejs.org/en
+
+npm install -g @vue/cli
+
+vue create vue-first-app
+
+extansion vetur
+
+
+
+"Parsing error: No babel config file detected" when IDE not open at Vue projects root directory
+
+https://stackoverflow.com/questions/71271760/parsing-error-no-babel-config-file-detected-when-ide-not-open-at-vue-projects
+
+"eslint.workingDirectories": [
+        {"mode": "auto"}
+ ],
+
+
+ To access settings.json file, click Ctrl+, or from, File > Preferences > Settings, then type eslint in the search bar, find Edit in settings.json in Options.
---
+++ vue3/startApp/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+    <li>
+        <h2>
+            {{ friend.name }}
+        </h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone: {{ friend.phone }}</li>
+            <li>Email: {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '92347123',
+                email: 'emael@laodc.eho'
+            },
+        }
+    },
+    methods: {
+        toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    },
+}
+
+</script>
