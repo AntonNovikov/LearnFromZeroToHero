9f67cb456c7ede242093e7b8dd65ac4f143d1959
Tony <un_razum@mail.ru>
2023-09-24 08:56:24 +0300

    feat: learn vuex

--- vue3/Academind/vuex-01/src/App.vue
+++ vue3/Academind/vuex-01/src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+  <h3>{{counter}}</h3>
+  <the-counter></the-counter>
+  <favorite-value></favorite-value>
+  <button @click="addOne">Add 10</button>
+
+  <change-counter></change-counter>
+</base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import TheCounter from './components/TheCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue
   },
+  computed:{
+    counter(){
+return  this.$store.getters.finalCounter
+    }
+  },
+  methods: {
+    addOne(){
+      // this.$store.commit('increase', {value:10});
+      // this.$store.commit({type: 'increase', value:10});
+      this.$store.dispatch({type: 'increase', value:10});
+    }
+  }
 };
 </script>
 
--- vue3/Academind/vuex-01/src/main.js
+++ vue3/Academind/vuex-01/src/main.js
@@ -1,7 +1,58 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      // setTimeout(function(){
+
+      //     state.counter = state.counter + 1;
+      // }, 2000); // not allowed
+      state.counter = state.counter + 1;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+  actions: {
+    increment(context) {
+      console.log(context);
+      setTimeout(function () {
+        context.commit('increment');
+      }, 2000);
+      // context.commit({type:'increment'});
+    },
+    increase(context,payload) {
+       context.commit('increase',payload)
+      },
+  },
+  getters: {
+    // finalCounter(state, getters){
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    // normalizedCounter(state,getters) {
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return '100+';
+      }
+      return finalCounter;
+    },
+  },
+});
+
 const app = createApp(App);
+app.use(store);
 
 app.mount('#app');
