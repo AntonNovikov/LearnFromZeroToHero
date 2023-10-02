1b4a82f1c78e0afd82a9c6e40d0e8d16c68643b2
Tony <un_razum@mail.ru>
2023-10-02 19:00:48 +0300

    feat: add fetching ( fetching only 60 sec or Refresh button)

--- vue3/Academind/main-prj/src/store/modules/coaches/actions.js
+++ vue3/Academind/main-prj/src/store/modules/coaches/actions.js
@@ -1,14 +1,62 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
+      //   id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
-      areas: data.areas
+      areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
-  }
+    const response = await fetch(
+      //   `https://coach-8fc28-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://coach-8fc28-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+    // const responseData = await response.json();
+    if (!response.ok) {
+      //error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+    // context.commit('registerCoach', coachData);
+  },
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
+    const response = await fetch(
+      //   `https://coach-8fc28-default-rtdb.firebaseio.com/coaches/${userId}.json`
+      `https://coach-8fc28-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
+  },
 };
