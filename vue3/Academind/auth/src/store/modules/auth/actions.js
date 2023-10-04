a8b03e3e76a7b2cc3f817a8f54b3ad57db21c7aa
Tony <un_razum@mail.ru>
2023-10-04 23:23:54 +0300

    feat: don't use it

--- vue3/Academind/auth/src/store/modules/auth/actions.js
+++ vue3/Academind/auth/src/store/modules/auth/actions.js
@@ -1,26 +1,19 @@
-// const API_KEY = 'AIzaSyBkyz-cUvvHhy8M5I04UlS3Ot-_FH5P744';
-
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBkyz-cUvvHhy8M5I04UlS3Ot-_FH5P744',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBkyz-cUvvHhy8M5I04UlS3Ot-_FH5P744', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
 
     const responseData = await response.json();
 
     if (!response.ok) {
       console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
+      const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
       throw error;
     }
 
@@ -28,29 +21,24 @@ export default {
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+      tokenExpiration: responseData.expiresIn
     });
   },
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBkyz-cUvvHhy8M5I04UlS3Ot-_FH5P744',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBkyz-cUvvHhy8M5I04UlS3Ot-_FH5P744', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
 
     const responseData = await response.json();
 
     if (!response.ok) {
       console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
+      const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
       throw error;
     }
 
@@ -58,7 +46,7 @@ export default {
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+      tokenExpiration: responseData.expiresIn
     });
-  },
+  }
 };
